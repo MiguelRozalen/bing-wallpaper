6fb7f049b8623276247952ce2f0caf5735c110b6
MiguelRozalen <mikelangel1993@gmail.com>
2017-08-07 11:44:24 +0200

    Resolution Image Fixed & Web Page Bugs Fixed II

--- docs/assets/js/bing-wallpaper.js
+++ docs/assets/js/bing-wallpaper.js
@@ -1,10 +1,6 @@
 $(function() {
-
     /*$.ajax({
       crossOrigin: true,
-
-    $.ajax({
-      //crossOrigin: true,
       url: "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US",
       success: function(data) {
 		var bingObject = JSON.parse(data);
@@ -103,6 +99,3 @@ function getFile(theURL, type, callback)
 		}
 	})
 };
-      }
-	});
-}); 
