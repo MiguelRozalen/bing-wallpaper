50cb80c29a8a007e6287f664a9786278c3ceda44
Miguel A. Rozalen <mikelangel1993@gmail.com>
2017-08-07 10:31:33 +0200

    Update bing-wallpaper.js

--- docs/assets/js/bing-wallpaper.js
+++ docs/assets/js/bing-wallpaper.js
@@ -1,6 +1,6 @@
 $(function() {
     $.ajax({
-      crossOrigin: true,
+      //crossOrigin: true,
       url: "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US",
       success: function(data) {
 		var bingObject = JSON.parse(data);
