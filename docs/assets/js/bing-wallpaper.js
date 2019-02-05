14c0161600a029162e8f9a2eb4870545eb853eca
Miguel A. Rozalen <mikelangel1993@gmail.com>
2019-02-05 09:52:12 +0100

    Update bing-wallpaper.js

--- docs/assets/js/bing-wallpaper.js
+++ docs/assets/js/bing-wallpaper.js
@@ -8,7 +8,6 @@ $(function() {
 		var imageUrl = "https://www.bing.com"+bingObject.images[0].url;
 		document.getElementById("hello").style.backgroundImage = "url('"+imageUrl+"')";
 		for(var i=0; i<7; i++){
-			console.log(images[i])
 			document.getElementById("div_img_"+i).style.backgroundImage = "url('https://www.bing.com"+images[i].urlbase+"_1920x1080.jpg')";
 			document.getElementById("a_img_"+i).setAttribute('href', "https://www.bing.com"+images[i].urlbase+"_1920x1080.jpg");
 			document.getElementById("text_img_"+i).innerHTML = images[i].copyright;
